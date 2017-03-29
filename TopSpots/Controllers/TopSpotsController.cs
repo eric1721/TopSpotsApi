4d2a15a6231f0a331bd53c82da6427d240d209a5
eric.ayres21 <eric.ayres21@gmail.com>
2017-03-29 16:19:18 -0700

    used api in csharp

---
+++ TopSpots/Controllers/TopSpotsController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.IO;
+using TopSpots.Models;
+using Newtonsoft.Json;
+
+namespace TopSpots.Controllers
+{
+    public class TopSpotsController : ApiController
+    { 
+        [HttpGet, Route("api/topspots")]
+        public IHttpActionResult GetTopSpots()
+        {
+            string topSpots = File.ReadAllText("C:\\dev\\TopSpots\\topspots.json");
+
+            TopSpot[] topSpotsArray = JsonConvert.DeserializeObject<TopSpot[]>(topSpots);
+
+            return Ok(topSpotsArray);
+        }
+    }
+}
